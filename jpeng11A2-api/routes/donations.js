781ae8688fa69eec16a82b4c37ad7641e5ea978c
JiyePeng <145826153+JiyePeng@users.noreply.github.com>
2024-10-12 20:10:59 +0800

    crate donation api

--- jpeng11A2-api/routes/donations.js
+++ jpeng11A2-api/routes/donations.js
@@ -21,4 +21,43 @@ router.get('/:fundraiserid', function(req, res, next) {
   );
 });
 
+/* Create a donation. */
+router.post('/', function(req, res, next) {
+  const amount = req.body.amount;
+  const giver = req.body.giver;
+  const fundraiserid = req.body.fundraiserid;
+
+  if (!amount) {
+    res.status(400).send({ error: "amount needed!" })
+    return
+  }
+
+  if (amount < 5) {
+    res.status(400).send({ error: "amount should over 5 aud!" })
+    return
+  }
+
+  if (!giver) {
+    res.status(400).send({ error: "giver needed!" })
+    return
+  }
+
+  if (!fundraiserid) {
+    res.status(400).send({ error: "fundraiserid needed!" })
+    return
+  }
+
+  connection.query(
+    'INSERT INTO DONATION(DATE, AMOUNT, GIVER, FUNDRAISER_ID) VALUES(?,?,?,?)',
+    [new Date(),amount,giver,fundraiserid],
+    function (err, results, fields) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(results); // results contains rows returned by server
+      }
+    }
+  );
+});
+
 module.exports = router;
